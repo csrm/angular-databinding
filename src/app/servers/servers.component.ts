9da07e42ded1f45377a09c0d1a750211f94612e0
srchivukula <schivuku@ford.com>
2023-12-03 14:41:18 +0530

    Implemented String Interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
- allowNewServer = false;
-
- constructor() {
-   setTimeout(() => {
-    this.allowNewServer = true;
-   },2000)
- }
 }
